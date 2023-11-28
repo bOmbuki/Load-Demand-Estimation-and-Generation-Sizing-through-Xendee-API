f30de87be69a185c84f8bcc1a27d9d61c61e67c3
bmogaka <bmogaka@asu.edu>
2023-11-28 15:12:42 -0700

    Updates to load demand estimation and main

--- api_Optimization.py
+++ api_Optimization.py
@@ -5,8 +5,8 @@ from requests.auth import HTTPBasicAuth
 """ API code to communicate with xendee servers to facilitate batch techno-economic analysis """
 
 xendeeAPIbaseUrl = "https://asu-api.xendee.com/api"
-xendeeAPIUsername = '402B4A23-1757-4AC9-9DBB-BA3427A410B0'
-xendeeAPIPassword = '05BF579D-55A1-43D5-A81D-81CDF59AE7B8'
+xendeeAPIUsername = # USERNAME SPECIFIED IN NOTEPAD ATTACHED IN A SEPERATE LINK
+xendeeAPIPassword = # PASSWORD SPECIFIED IN NOTEPAD ATTACHED IN A SEPERATE LINK
 xendeeAPIUserId = 'bombuki@asu.edu'  # use your ASU XENDEE login email/user ID
 
 basic = HTTPBasicAuth(xendeeAPIUsername, xendeeAPIPassword)
